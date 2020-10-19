5e96ac102ac7911e0e94eb807c061946cf125768
Emmanuel Blot <emmanuel.blot@free.fr>
2020-10-19 16:25:25 +0200

    Update RISC-V binutils to 2.35.1 ans use out-of-source build

--- Formula/riscv-elf-binutils.rb
+++ Formula/riscv-elf-binutils.rb
@@ -3,21 +3,23 @@ require "formula"
 class RiscvElfBinutils < Formula
   desc "GNU Binutils for OS-less RISC-V architecture"
   homepage "https://www.gnu.org/software/binutils/"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.35.tar.xz"
-  sha256 "1b11659fb49e20e18db460d44485f09442c8c56d5df165de9461eb09c8302f85"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.xz"
+  sha256 "3ced91db9bf01182b7e420eab68039f2083aed0a214c0424e257eae3ddee8607"
 
   depends_on "gmp"
   depends_on "mpfr"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--target=riscv64-unknown-elf",
-                "--disable-shared", "--disable-nls",
-                "--with-gmp=#{Formulary.factory("gmp").prefix}",
-                "--with-mpfr=#{Formulary.factory("mpfr").prefix}",
-                "--disable-cloog-version-check",
-                "--enable-multilibs", "--enable-interwork", "--enable-lto",
-                "--disable-werror", "--disable-debug"
-    system "make"
-    system "make install"
+    mkdir "build" do
+      system "../configure", "--prefix=#{prefix}", "--target=riscv64-unknown-elf",
+                  "--disable-shared", "--disable-nls",
+                  "--with-gmp=#{Formulary.factory("gmp").prefix}",
+                  "--with-mpfr=#{Formulary.factory("mpfr").prefix}",
+                  "--disable-cloog-version-check",
+                  "--enable-multilibs", "--enable-interwork", "--enable-lto",
+                  "--disable-werror", "--disable-debug"
+      system "make"
+      system "make install"
+    end
   end
 end
