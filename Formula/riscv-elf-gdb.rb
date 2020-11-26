c821bc9590be9676bc6c9fbfe46e62e94d9e2c4e
Emmanuel Blot <emmanuel.blot@free.fr>
2020-11-26 11:09:28 +0100

    Upgrade to GDB 10.1 and fix a build issue on macOS 11 (Big Sur)

--- Formula/riscv-elf-gdb.rb
+++ Formula/riscv-elf-gdb.rb
@@ -3,19 +3,26 @@ require "formula"
 class RiscvElfGdb < Formula
   desc "GNU debugger for RISC-V architecture"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-9.2.tar.xz"
-  sha256 "360cd7ae79b776988e89d8f9a01c985d0b1fa21c767a4295e5f88cb49175c555"
+  url "http://ftp.gnu.org/gnu/gdb/gdb-10.1.tar.xz"
+  sha256 "f82f1eceeec14a3afa2de8d9b0d3c91d5a3820e23e0a01bbb70ef9f0276b62c0"
 
   depends_on "gmp"
   depends_on "libmpc"
   depends_on "mpfr"
   depends_on "readline"
+  depends_on "python@3.8"
 
   # Linux dependencies.
-  depends_on "python" unless OS.mac?
   depends_on "guile" unless OS.mac?
 
+  # need to regenerate configure script after applying patch
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  patch :DATA
+
   def install
+    system "autoreconf gdb"
     mkdir "build" do
       system "../configure", "--prefix=#{prefix}",
                 "--target=riscv64-unknown-elf",
@@ -26,9 +33,22 @@ class RiscvElfGdb < Formula
                 "--with-python",
                 "--without-cloog",
                 "--enable-lto", "--disable-werror"
-      system "false"
       system "make"
       system "make install"
     end
   end
 end
+
+# This patch addresses a configuration issue where GDB does not include
+# string.h, which breaks build as strncmp is no longer declared on macOS 11
+__END__
+--- a/gdb/acinclude.m4  2020-11-26 10:10:52.000000000 +0100
++++ b/gdb/acinclude.m4  2020-11-26 10:12:25.000000000 +0100
+@@ -362,6 +362,7 @@
+   AC_CACHE_CHECK([$1], [$2],
+   [AC_TRY_LINK(
+   [#include <stdlib.h>
++  #include <string.h>
+   #include "bfd.h"
+   #include "$4"
+   ],
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # homebrew-riscvelf
+
 Homebrew tap for RISC-V toolchain
+
+### Note
+
+To install a version of openocd supporting RISCV, install the development
+version:
+
+    brew install --HEAD open-ocd
