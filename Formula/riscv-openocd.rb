b05b0465d2b8113937b4e85ae5e9aa2439881202
Emmanuel Blot <emmanuel.blot@free.fr>
2022-10-17 12:02:41 +0200

    openocd: update to v0.12.0-rc1 and enable support for HiFive SPI flash

--- Formula/riscv-openocd.rb
+++ Formula/riscv-openocd.rb
@@ -1,13 +1,13 @@
 class RiscvOpenocd < Formula
   desc "On-chip debugging, in-system programming for RISC-V"
   homepage "https://sourceforge.net/projects/openocd/"
-
-  version="0.11.0_52177592f"  # 2022/08/01
-  revision="52177592f9d3afc6a008f8e1b321cf74e823018f"
+  # url "https://github.com/openocd-org/openocd/archive/refs/tags/v0.12.0-rc1.tar.gz"
+  # sha256 "dcf00672cbc72c17ab78596aa486a953766f05146b0ca4a1cfe8ae894bf75cc6"
+  # version="0.12.0-rc1"
 
   head do
-    url "https://github.com/riscv/riscv-openocd.git",
-        :revision => "#{revision}"
+    url "https://github.com/openocd-org/openocd.git", :tag => "v0.12.0-rc1"
+    patch :DATA
   end
 
   depends_on "autoconf" => :build
@@ -30,6 +30,7 @@ class RiscvOpenocd < Formula
       --enable-verbose-usb-comms
       --enable-ftdi
       --enable-jlink
+      --enable-remote-bitbang
       --disable-doxygen-html
       --disable-doxygen-pdf
       --disable-werror
@@ -65,7 +66,6 @@ class RiscvOpenocd < Formula
       --disable-buspirate
       --disable-sysfsgpio
       --disable-minidriver-dummy
-      --disable-remote-bitbang
     ]
 
     system "./bootstrap"
@@ -74,3 +74,17 @@ class RiscvOpenocd < Formula
     system "make", "install"
   end
 end
+
+__END__
+diff --git a/src/flash/nor/spi.c b/src/flash/nor/spi.c
+index eed747b58..83075c281 100644
+--- a/src/flash/nor/spi.c
++++ b/src/flash/nor/spi.c
+@@ -113,6 +113,7 @@ const struct flash_device flash_devices[] = {
+ 	FLASH_ID("gd gd25q256c",        0x13, 0x00, 0x12, 0xdc, 0xc7, 0x001940c8, 0x100, 0x10000, 0x2000000),
+ 	FLASH_ID("gd gd25q512mc",       0x13, 0x00, 0x12, 0xdc, 0xc7, 0x002040c8, 0x100, 0x10000, 0x4000000),
+ 	FLASH_ID("issi is25lp032",      0x03, 0x00, 0x02, 0xd8, 0xc7, 0x0016609d, 0x100, 0x10000, 0x400000),
++	FLASH_ID("issi is25lq040b",     0x13, 0xeb, 0x02, 0xd8, 0xc7, 0x0013409d, 0x100, 0x10000, 0x80000),
+ 	FLASH_ID("issi is25lp064",      0x03, 0x00, 0x02, 0xd8, 0xc7, 0x0017609d, 0x100, 0x10000, 0x800000),
+ 	FLASH_ID("issi is25lp128d",     0x03, 0xeb, 0x02, 0xd8, 0xc7, 0x0018609d, 0x100, 0x10000, 0x1000000),
+ 	FLASH_ID("issi is25wp128d",     0x03, 0xeb, 0x02, 0xd8, 0xc7, 0x0018709d, 0x100, 0x10000, 0x1000000),
